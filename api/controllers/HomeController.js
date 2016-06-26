f58fc803a19e233b20743f68bf2fbd059856ff7d
koolay <koolay@163.com>
2016-06-26 14:51:05 +0800

    add api model

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -4,49 +4,30 @@
  * @description :: Server-side logic for managing homes
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
-/**
-// With a JSON doc
-Person.
-  find({
-    occupation: /host/,
-    'name.last': 'Ghost',
-    age: { $gt: 17, $lt: 66 },
-    likes: { $in: ['vaporizing', 'talking'] }
-  }).
-  limit(10).
-  sort({ occupation: -1 }).
-  select({ name: 1, occupation: 1 }).
-  exec(callback);
-
-// Using query builder
-Person.
-  find({ occupation: /host/ }).
-  where('name.last').equals('Ghost').
-  where('age').gt(17).lt(66).
-  where('likes').in(['vaporizing', 'talking']).
-  limit(10).
-  sort('-occupation').
-  select('name occupation').
-  exec(callback);
- *
- */
 module.exports = {
     index: function(req, res) {
         return res.view('index');
     },
     create: function(req, res) {
-        Apis.create({
+        var api = new Apis({
             title: 'test',
             author: 'mysoft',
             body: 'hello, welcome',
             hidden: false
-        }, function(err, api) {
+        });
+        var error = api.validateSync();
+        if (error) {
+            return res.json({
+                result: false,
+                error: error
+            });
+        }
+        api.save(function(err, api) {
             if (err) {
                 return res.negotiate(err);
             } else {
                 return res.json(api);
-            }
+            };
         });
     },
 
--- api/models/Apis.js
+++ api/models/Apis.js
@@ -4,47 +4,86 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var subKeyValue = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: String,
+    format: {
+        type: String,
+        default: 'string'
+    },
+    length: Number,
+    required: {
+        type: Boolean,
+        required: true
+    }
+}, {
+    _id: false
+});
+
 module.exports = {
 
-  schema: {
-    title:  String,
-    author: String,
-    body:   String,
-    date: { type: Date, default: Date.now },
-    hidden: Boolean,
-    meta: {
-      votes: Number,
-      favs:  Number
-    }
-  },
+    schema: {
+        version: String,
+        summary: {
+            type: String,
+            required: true
+        },
+        description: String,
+        method: {
+            type: String,
+            required: true,
+            enum: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
+        }, //GET/POST/PUT/
+        path: {
+            type: String,
+            required: true,
+            match: '/^(\/\w+)+/'
+        },
+        headers: [subKeyValue],
+        parameters: [subKeyValue],
+        response: {
+            httpCode: {
+                type: Number,
+                required: true,
+                min: 200
+            },
+            data: Schema.Types.Mixed
+
+        }
+    },
 
 
 
-  /**
-   * constructSchema()
-   *
-   * Note that this function must be synchronous!
-   *
-   * @param  {Dictionary} schemaDefinedAbove  [the raw schema defined above, or `{}` if no schema was provided]
-   * @param  {SailsApp} sails                 [just in case you have globals disabled, this way you always have access to `sails`]
-   * @return {MongooseSchema}
-   */
-  //constructSchema: function (schemaDefinedAbove, sails) {
+    /**
+     * constructSchema()
+     *
+     * Note that this function must be synchronous!
+     *
+     * @param  {Dictionary} schemaDefinedAbove  [the raw schema defined above, or `{}` if no schema was provided]
+     * @param  {SailsApp} sails                 [just in case you have globals disabled, this way you always have access to `sails`]
+     * @return {MongooseSchema}
+     */
+    //constructSchema: function (schemaDefinedAbove, sails) {
     //// e.g. we might want to pass in a second argument to the schema constructor
     //var newSchema = new sails.mongoose.Schema(schemaDefinedAbove, { autoIndex: false });
 
     //// Or we might want to define an instance method:
     //newSchema.method('meow', function () {
-      //console.log('meeeeeoooooooooooow');
+    //console.log('meeeeeoooooooooooow');
     //});
 
     //// Or a static ("class") method:
     //newSchema.static('findByName', function (name, callback) {
-      //return this.find({ name: name }, callback);
+    //return this.find({ name: name }, callback);
     //});
 
     //// Regardless, you must return the instantiated Schema instance.
     //return newSchema;
-  //}
+    //}
 
 };
