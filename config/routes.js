3957606fae63014ff94022518e0e7e16020e758a
koolay <koolay@163.com>
2016-07-02 04:17:12 +0800

    change route

--- config/routes.js
+++ config/routes.js
@@ -1,28 +1,31 @@
 module.exports.routes = {
 
-  '/': 'HomeController.index',
+    'post /login': 'UsersController.login',
+    'post /signup': 'UsersController.signup',
 
-  'post /login': 'UsersController.login',
-  'post /signup': 'UsersController.signup',
+    //path
+    'get /api/apis': 'ApiController.list',
+    'get /api/apis/:id': 'ApiController.detail',
+    'post /api/apis': 'ApiController.create',
+    'get /api/swagger/:id': 'ApiController.swagger',
 
-  //path
-  'get /api/apis': 'ApiController.list',
-  'get /api/apis/:id': 'ApiController.detail',
-  'post /api/apis': 'ApiController.create',
-  'get /api/swagger/:id': 'ApiController.swagger',
+    //mock
+    'get /api/mocks': 'MockController.list',
+    'post /api/mocks': 'MockController.create',
+    'delete /api/mock/remove': 'MockController.remove',
+    '/mock/:pathname': 'MockController.view',
+    'get /mock/test': 'MockController.test',
 
-  //mock
-  'get /api/mocks': 'MockController.list',
-  'post /api/mocks': 'MockController.create',
-  'delete /api/mock/remove': 'MockController.remove',
-  '/mock/:pathname':'MockController.view',
-  'get /mock/test':'MockController.test',
+    //docs
+    'get /docs/:project_id': 'DocsController.view',
 
-  //docs
-  'get /docs/:project_id': 'DocsController.view',
-
-  //创建项目
-  'post /projects': 'ProjectController.create',
-  'get /projects/:id': 'ProjectController.swagger'
+    //创建项目
+    'post /projects': 'ProjectController.create',
+    'get /projects/:id': 'ProjectController.swagger',
+    '/*': {
+        view: 'index',
+        skipAssets: true,
+        skipRegex: /(^\/api\/.*$)|(^\/docs\/.*$)|(^\/mock\/.*$)/,
+    },
 
 };
