151d031f6b7dc288cfd9aa1f196a36c4adfd3b49
yulingchen <yulingchen@vip.qq.com>
2016-07-04 11:12:53 +0800

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,15 +3,12 @@ module.exports.mongoose = {
   // The Mongo connection URI
   // (see https://github.com/mikermcneil/sails-hook-orm-mongoose/blob/master/README.md#configuration for more info)
   //
-  uri: 'mongodb://10.5.24.46:27017/apigo',
+  uri: 'mongodb://apigo:apigo2016@ds040089.mlab.com:40089/apigo',
   connectionOpts: {
-      db: { native_parser: true },
-      server: { poolSize: 5 },
-      replset: { rs_name: 'apigo' },
-      //user: 'apigo',
-      //pass: 'apigo2016'
-
+       db: { native_parser: true },
+       server: { poolSize: 5 },
+       replset: { rs_name: 'apigo' },
+       user: 'apigo',
+       pass: 'apigo2016'
   },
-
-
 };
